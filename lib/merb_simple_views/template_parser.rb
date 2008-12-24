48cd8066f4996520d2c40e70392d0e43161d4345
mynyml <mynyml@gmail.com>
2008-12-23 21:11:05 -0500

    Added basic template parser class.

--- lib/merb_simple_views/template_parser.rb
+++ lib/merb_simple_views/template_parser.rb
@@ -1,6 +1,8 @@
 module SimpleViews
   class TemplateParser
     attr_accessor :raw_templates
+    alias :raw  :raw_templates
+    alias :raw= :raw_templates=
 
     # Read a file and fetch its __END__ section, if any
     #
@@ -8,9 +10,30 @@ module SimpleViews
     # file<String,Pathname>:: File to find in-file templates in
     #
     # :api: public
-    def initialize(file)
-      parts = Pathname(file).read.split('__END__')
-      self.raw_templates = parts.size > 1 ? parts.last : nil
+    def initialize(file='')
+      file = Pathname(file)
+      if file.exist?
+        parts = file.read.split(/^__END__$/)
+        if parts.size > 1
+          self.raw_templates = parts[1].lstrip
+        end
+      end
+    end
+
+    def parse
+      templates, current, ignore = {}, nil, false
+      self.raw_templates.each_line do |line|
+        if matches = line.strip.match(/^\s*(\#)*\s*@@\s*(.*)/)
+          ignore = (matches[1] && matches[1].match(/^#/)) and next #skip commented out templates
+          templates[current = matches[2]] = ''
+        elsif ignore
+          next
+        elsif current
+          templates[current] << line
+        end
+      end
+      # remove trailing witespace off every template body and template name
+      templates.each {|key,value| templates[key.chomp.gsub(/[[:blank:]]|\t/,'')] = value.rstrip }
     end
   end
 end
--- spec/template_parser_spec.rb
+++ spec/template_parser_spec.rb
@@ -2,9 +2,49 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Parser" do
 
+  before(:each) do
+    @parser = SimpleViews::TemplateParser.new(__FILE__)
+  end
+
   it "should load in-file templates" do
-    parser = SimpleViews::TemplateParser.new(__FILE__)
-    parser.raw_templates.should match(/ohaie/)
+    @parser.raw_templates.should match(/ohaie/)
+  end
+
+  describe "when parsing template strings" do
+
+    it "should split templates at @@ marks" do
+      @parser.raw = %|
+        @@ index
+        kittehs
+        @@ show
+        kitteh1
+      |.unindent
+      @parser.parse.size.should == 2
+      @parser.parse['index'].strip.should == 'kittehs'
+      @parser.parse['show'].strip.should  == 'kitteh1'
+    end
+
+    it "should remove trailing witespace off every template" do
+      @parser.raw = %|
+        @@ index
+
+        kittehs \t\n
+      |.unindent
+      @parser.parse['index'].should_not match(/\t\n$/)
+      @parser.parse['index'].should     match(/^\n/)
+    end
+
+    it "should not be bothered by amount of spaces or tabs between marker and template name" do
+      @parser.raw = %|
+        @@index
+        kittehs
+        @@\t\sshow
+        kitteh1
+      |.unindent
+      @parser.parse['index'].should_not be_nil
+      @parser.parse['show'].should_not be_nil
+      @parser.parse["\t\sshow"].should be_nil
+    end
   end
 end
 
