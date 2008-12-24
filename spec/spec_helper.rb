e2aee29a2da601b2fcd2a8da7445ef8879353da1
mynyml <mynyml@gmail.com>
2008-12-23 21:09:49 -0500

    Added string unindent test helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,17 @@ Merb.start :environment => 'test', :adapter => 'runner'
 Spec::Runner.configure do |config|
   config.include Merb::Test::RequestHelper  
 end
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Core Extensions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This might cause errors to slip into specs, but it beats using fixtures.
+class String
+  def unindent
+    indent = self.select {|line| !line.strip.empty? }.map {|line| line.index(/[^\s]/) }.compact.min
+    self.gsub(/^[[:blank:]]{#{indent}}/, '')
+  end
+  def unindent!
+    self.replace(self.unindent)
+  end
+end
