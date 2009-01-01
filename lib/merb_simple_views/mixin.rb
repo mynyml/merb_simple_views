62f4469b0138506443d3287af2444ea90797cf1f
mynyml <mynyml@gmail.com>
2009-01-01 08:56:14 -0200

    Added documentation to mixin and template parser.

--- lib/merb_simple_views/mixin.rb
+++ lib/merb_simple_views/mixin.rb
@@ -2,6 +2,8 @@ module SimpleViews
 
   TEMPLATES = {}
 
+  # Adds SimpleViews functionality to a Merb controller, allowing templates
+  # defined in the controller's file to serve as views. See README for more.
   module Mixin
     attr_reader :_template_parser
 
@@ -10,6 +12,30 @@ module SimpleViews
       super
     end
 
+    # In addition to default #render behaviour, parse simple views defined in
+    # controller's file and render them when requested, or render external
+    # templates otherwise.
+    #
+    # ==== Notes
+    # 1. Turned into a chainable method in merb_simple_views.rb
+    # 2. Uses a VirtualFile to trick template engines.
+    #
+    # ==== Parameters
+    # See Merb::RenderMixin#render
+    #
+    # ==== Options
+    # See Merb::RenderMixin#render
+    #
+    # ==== Returns
+    # See Merb::RenderMixin#render
+    #
+    # ==== Raises
+    # See Merb::RenderMixin#render
+    #
+    # ==== Alternatives
+    # See Merb::RenderMixin#render
+    #
+    # :api: public
     def render(*args)
       tpls_file = (@__caller_info__ || __caller_info__).first.first
       self._template_parser.load(tpls_file).parse.each do |template_name, raw_content|
@@ -22,6 +48,28 @@ module SimpleViews
       super
     end
 
+    # Wrap #display and store caller info so that #render (called internally by
+    # #display) knows what file to fetch template data from.
+    #
+    # ==== Notes
+    # Turned into a chainable method in merb_simple_views.rb
+    #
+    # ==== Parameters
+    # See Merb::RenderMixin#display
+    #
+    # ==== Options
+    # See Merb::RenderMixin#display
+    #
+    # ==== Returns
+    # See Merb::RenderMixin#display
+    #
+    # ==== Raises
+    # See Merb::RenderMixin#display
+    #
+    # ==== Alternatives
+    # See Merb::RenderMixin#display
+    #
+    # :api: public
     def display(*args)
       @__caller_info__ = __caller_info__
       super
--- lib/merb_simple_views/template_parser.rb
+++ lib/merb_simple_views/template_parser.rb
@@ -1,24 +1,26 @@
 module SimpleViews
   class TemplateParser
+    # templates as a string, directly extracted from the file.
     attr_accessor :raw_templates
     alias :raw    :raw_templates
     alias :raw=   :raw_templates=
 
+    # templates as a {'name' => 'content'} hash
     attr_accessor  :parsed_templates
     alias :parsed  :raw_templates
     alias :parsed= :raw_templates=
 
     def initialize
-      self.raw_templates = ''
+      self.raw_templates    = ''
       self.parsed_templates = {}
     end
 
-    # Read a file and fetch its __END__ section, if any
+    # Read a file and fetch its __END__ section, if any.
     #
-    # === Parameters
+    # ==== Parameters
     # file<String,Pathname>:: File to find in-file templates in
     #
-    # === Returns
+    # ==== Returns
     # self
     #
     # :api: public
@@ -33,6 +35,43 @@ module SimpleViews
       self
     end
 
+    # Parse templates string and extract template names and contents.
+    #
+    # Template names must be preceded with '@@', appear alone on their line,
+    # and follow the same conventions regular templates do. They can also be
+    # disabled by simply commenting out their name.
+    #
+    # ==== Parameters
+    # file<String,Pathname>:: File to find in-file templates in
+    #
+    # ==== Returns
+    # Hash:: Parsed templates as {'name' => 'content'}
+    #
+    # ==== Examples
+    #
+    #   #posts_controller.rb
+    #   [...]
+    #   __END__
+    #   @@ index.html.erb
+    #   posts
+    #
+    #   @@ show.html.erb
+    #   post01
+    #
+    #   #=> {'index.html.erb' => "posts", 'show.html.erb' => "post01"}
+    #
+    # To disable a template, comment out its name:
+    #
+    #   __END__
+    #   #@@ index.html.erb
+    #   posts
+    #
+    #   #=> {}
+    #
+    # ==== Notes
+    # Also stores the result in self.parsed_templates
+    #
+    # :api: public
     def parse(file='')
       self.load(file) if file
       templates, current, ignore = {}, nil, false
